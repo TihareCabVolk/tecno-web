6c3a41e7981831ec41a45c07fe97df279d294acf
Tihare-Cabello <tihare.cab@gmail.com>
2024-10-26 14:47:38 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { CarrritoCompraComponent } from './carrito-compra/carrrito-compra.component';
+import { HomeComponent } from './home/home.component';
+import { CarritoListarComponent } from './carrito-listar/carrito-listar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'carrito-listar', component: CarritoListarComponent },
+    { path: '**', pathMatch: 'full', redirectTo: '' }
+
+];
