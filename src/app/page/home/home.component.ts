61b704e61d33c0db2fbd0d74175db77d4b7c1cee
Cayupi <sharksebadbd@gmail.com>
2024-11-02 16:59:30 -0300

    Sobre Nosotros
    Se creó un componente "about-us" con una pequeña sección de información sobre la compañía

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from '../../componets/navbar/navbar.component';
 import { CarosuelComponent } from '../../componets/carosuel/carosuel.component';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from '../../componets/footer/footer.component';
+import { AboutUsComponent } from "../../componets/about-us/about-us.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,CarosuelComponent,CommonModule,FooterComponent],
+  imports: [NavbarComponent, CarosuelComponent, CommonModule, FooterComponent, AboutUsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
