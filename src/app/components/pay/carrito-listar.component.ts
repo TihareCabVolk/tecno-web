ff028eb0856b48f7442d7145ce12c41c7b625d4a
Tihare-Cabello <tihare.cab@gmail.com>
2024-11-09 23:58:43 -0300

    Cambios metodo de pago

--- src/app/components/pay/carrito-listar.component.ts
+++ src/app/components/pay/carrito-listar.component.ts
@@ -6,8 +6,11 @@ import { FormsModule } from '@angular/forms';
 import { initFlowbite } from 'flowbite';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { VisibilidadElementosService } from '../../services/visibilidad-elementos.service';
+import { trigger, transition, style, animate } from '@angular/animations'
+
 import { AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-cart-listar',
@@ -15,6 +18,17 @@ import * as L from 'leaflet';
   imports: [CommonModule, FormsModule, NavbarComponent],
   templateUrl: './carrito-listar.component.html',
   styleUrl: './carrito-listar.component.scss',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-100%)' }),
+        animate('300ms 0ms ease-in', style({ opacity: 1, transform: 'translateY(0)' }))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-out', style({ opacity: 0, transform: 'translateY(-100%)' }))
+      ])
+    ])
+  ]
 })
 export class CarritoListarComponent implements OnInit {
   public carritoService = inject(CarritoService);
@@ -25,43 +39,44 @@ export class CarritoListarComponent implements OnInit {
   public botonRetiro: boolean = true;
 
   public isModalVisible: boolean = false;
+  public isVisiblePago: boolean = false;
 
   @Input() isVisible: boolean = false;
   @Output() close = new EventEmitter<void>();
   @Input() deliveryVisible: boolean = false;
   @Input() retiroVisible: boolean = false;
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     initFlowbite();
     this.getListCarrito();
     this.estadoService.setMostrar(false);
   }
 
-  getListCarrito() {
+  public getListCarrito() {
     this.listCarrito = this.carritoService.getCarrito();
   }
 
-  eliminarItem(index: number) {
+  public eliminarItem(index: number) {
     this.carritoService.eliminar(index);
     this.getListCarrito();
   }
 
-  actualizar(item: Carrito, index: number) {
+  public actualizar(item: Carrito, index: number) {
     this.carritoService.actualizar(index, item.cantidad);
   }
 
-  onKeyDown(event: any) {
+  public onKeyDown(event: any) {
     event.preventDefault();
   }
 
   item = { cantidad: 1 };
 
-  incrementar(item: Carrito, index: number) {
+  public incrementar(item: Carrito, index: number) {
     item.cantidad += 1;
     this.actualizar(item, index);
   }
 
-  decrementar(item: Carrito, index: number) {
+  public decrementar(item: Carrito, index: number) {
     if (item.cantidad > 1) {
       item.cantidad -= 1;
       this.actualizar(item, index);
@@ -69,31 +84,39 @@ export class CarritoListarComponent implements OnInit {
   }
 
   // Se hace visible la ventana para la forma de retiro del pedido o delivery
-  showModal(): boolean {
+  public showModal(): boolean {
     return this.isModalVisible = true;
   }
 
   // Se hace invisible la ventana para la forma de retiro del pedido o delivery
-  hideModal(): boolean {
+  public hideModal(): boolean {
     return this.isModalVisible = false;
   }
 
   // Cerrar la ventana flotante con un click fuera de ella
-  cerrarModalConClick(event: MouseEvent) {
-    if (event.target === event.currentTarget) {
-      this.isModalVisible = false;
-    }
+  public cerrarModalConClick(event: MouseEvent): boolean {
+    if (event.target === event.currentTarget) { return this.isModalVisible = false }
+    return this.isModalVisible
   }
 
   // Se hace visible el contenido del Delivery y se hace inaccesible el boton Retiro en Tienda
-  deliveryShow(): boolean {
+  public deliveryShow(): boolean {
     if (this.deliveryVisible == false) { this.botonDelivery = !this.botonDelivery; return this.deliveryVisible = true }
     else this.botonDelivery = !this.botonDelivery; return this.deliveryVisible = false;
   }
 
   // Se hace visible el contenido del Retiro en tienda y se hace inaccesible el boton Delivery
-  retiroShow(): boolean {
+  public retiroShow(): boolean {
     if (this.retiroVisible == false) { this.botonRetiro = !this.botonRetiro; return this.retiroVisible = true }
     else this.botonRetiro = !this.botonRetiro; return this.retiroVisible = false;
   }
+
+  public metodoPagoVisible(): boolean {
+    return this.isVisiblePago = true;
+  }
+
+  public metodoPagoNoVisible(): boolean {
+    return this.isVisiblePago = false;
+  }
+
 }
