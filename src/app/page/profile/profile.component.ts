451b5458e3e925516f4de813f79f02fc51e404e3
Cayupi <sharksebadbd@gmail.com>
2024-11-13 17:40:31 -0300

    profile implementado
    se han dejado valores por defecto porque no pude usar la BD
    objetivo del siguiente commit: uso correcto de la BD y colocar plantillas para cupones y pedidos

--- src/app/page/profile/profile.component.ts
+++ src/app/page/profile/profile.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  public email: string | null = null;
+  public email: string | null = 'Wonald@Donalds.wc';
+  public username: string | null = 'Wonald Wc Donalds';
+  public phone: string | null = '(+56) 9 1234 5678';
+  public selectedOption: string = 'perfil';
 
   constructor(private authService: AuthService, private router: Router) {}
 
@@ -20,9 +24,32 @@ export class ProfileComponent implements OnInit {
       if (authenticated) {
         this.router.navigate(['/login']); // Redirigir si no está logueado
       } else {
-        // Obtener el email del usuario
+        // Obtener el email, nombre de usuario y teléfono del usuario
+
+        /*
         this.email = this.authService.getEmail(); // Obtener el email
+        this.username = this.authService.getUsername(); // Obtener el nombre de usuario
+        this.phone = this.authService.getPhone(); // Obtener el teléfono
+        */
+       
       }
     });
+  }
+
+  profileOption(option: string) {
+    this.selectedOption = option;
+  }
+  
+  getContent() {
+    switch (this.selectedOption) {
+      case 'perfil':
+        return 'Contenido de Mi Perfil.ts';
+      case 'pedidos':
+        return 'Contenido de Mis Pedidos.ts';
+      case 'cupones':
+        return 'Contenido de Mis Cupones.ts';
+      default:
+        return 'Contenido de Mi Perfil.ts';
+    }
   }
 }
