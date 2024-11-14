9854477be0f776fd36f7c8469798996f37468ac6
Tihare-Cabello <tihare.cab@gmail.com>
2024-11-14 01:47:06 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { CarritoListarComponent } from './components/pay/carrito-listar.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CatalogoComponent],
+  imports: [RouterOutlet, CatalogoComponent, CarritoListarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
