a32551c728cf845e86501b5ab18774371f5d6f5b
Byron-Santibanez <byron.santibanez.fernandez@alumnos.uta.cl>
2024-11-24 15:34:09 -0300

    version con bd

--- src/app/services/pedidos.service.ts
+++ src/app/services/pedidos.service.ts
@@ -3,13 +3,15 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class PedidosService {
-  private apiUrl = 'http://localhost:4200/api/pedidos'; // Cambia el puerto si es necesario
+
+  private apiUrl = 'http://localhost:3000/orders'; // Cambia a la URL de tu API
 
   constructor(private http: HttpClient) {}
 
+  // Obtener pedidos desde la API
   getPedidos(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
   }
