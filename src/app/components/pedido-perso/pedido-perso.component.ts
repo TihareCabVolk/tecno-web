2d608368fa25717a0a85279b51d457d02c578a55
Tihare-Cabello <tihare.cab@gmail.com>
2024-11-25 01:29:05 -0300

    Update pedido-perso.component.ts

--- src/app/components/pedido-perso/pedido-perso.component.ts
+++ src/app/components/pedido-perso/pedido-perso.component.ts
@@ -10,7 +10,7 @@ import { ProductoService } from '../../services/producto.service';
 @Component({
   selector: 'app-pedido-perso',
   standalone: true,
-  imports: [CommonModule, FormsModule, ProductsComponent],
+  imports: [CommonModule, FormsModule],
   templateUrl: './pedido-perso.component.html',
   styleUrl: './pedido-perso.component.scss'
 })
@@ -20,6 +20,7 @@ export class PedidoPersoComponent implements OnInit {
 
   @Input() products: Product[] = [];
   @Input() modo: string = '';
+  @Input() producto: Product | null = null;
   @Output() close = new EventEmitter<void>();
 
   public tamanoSeleccionado: number = 0;
